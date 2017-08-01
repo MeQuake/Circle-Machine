3fb9d81d46220702c9dc6e3beaaf9d01bbf0df56
Adam Mankowski <maniekmanfred69@gmail.com>
2017-08-01 20:43:09 +0200

    Add backgrounds and brightness change on mouse enter

--- js/script.js
+++ js/script.js
@@ -7,10 +7,14 @@ function initializeEvents() {
   for(var i = 0; i < inlines.length; i++) {
     inlines[i].addEventListener('mouseover', function() {
       var circle = this.getElementsByClassName('circle')[0];
+      var background = this.getElementsByClassName('background')[0];
+      background.classList.add('backgroundTouched');
       circle.classList.add('touched');
     });
     inlines[i].addEventListener('mouseout', function() {
       var circle = this.getElementsByClassName('circle')[0];
+      var background = this.getElementsByClassName('background')[0];
+      background.classList.remove('backgroundTouched');
       circle.classList.remove('touched')
     });
   }
