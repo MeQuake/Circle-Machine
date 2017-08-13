3978599002fd11224ef86d22abc0e10c634f0c57
Adam Mankowski <maniekmanfred69@gmail.com>
2017-08-13 14:54:41 +0200

    Add opacity transition to 'details' element

--- js/script.js
+++ js/script.js
@@ -8,14 +8,18 @@ function initializeEvents() {
     inlines[i].addEventListener('mouseover', function() {
       var circle = this.getElementsByClassName('circle')[0];
       var background = this.getElementsByClassName('background')[0];
+      var details = this.getElementsByClassName('details')[0];
       background.classList.add('backgroundTouched');
       circle.classList.add('touched');
+      details.classList.add('touched');
     });
     inlines[i].addEventListener('mouseout', function() {
       var circle = this.getElementsByClassName('circle')[0];
       var background = this.getElementsByClassName('background')[0];
+      var details = this.getElementsByClassName('details')[0];
       background.classList.remove('backgroundTouched');
       circle.classList.remove('touched')
+      details.classList.remove('touched');
     });
   }
 }
